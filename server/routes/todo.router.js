3066457b35996734028ed444383b70c9a3cfa6e2
Ashley Wessel <amerino.dance@gmail.com>
2023-10-23 20:48:55 -0500

    added a GET route to the todo.router.js

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,6 +4,19 @@ const pool = require('../modules/pool.js');
 
 // GET
 
+router.get('/', (req, res) => {
+
+    let queryText = 'SELECT * from "tasklist" ORDER BY "id" ASC;';
+    pool.query(queryText)
+        .then(result => {
+            res.send(result.rows);
+        }).catch(error => {
+            console.error('ERROR IN GET /items', error);
+            res.sendStatus(500);
+        });
+})
+
+
 // POST
 
 // PUT
